7de0e89d4bdfa52f57a789a90404556a6405fad1
mustalidhanerawala <dhanerawala5@hotmail.com>
2024-11-27 19:18:21 +0530

    test4

--- homepage.js
+++ homepage.js
@@ -6,25 +6,32 @@ window.addEventListener("scroll", function () {
 
     // Get multiple elements with querySelectorAll
     const showimg = document.querySelectorAll("#box1, #box3, #box5");
+    const descpara = document.querySelectorAll("#box2, #box4, #box6");
 
     // Apply animation for #the-exclusive element
-    if (excPosition < window.innerHeight && excPosition > 0) 
-    {
-        exc.style.animation = "entry 1s ease-out forwards";
-    } 
-    else 
-    {
+    if (excPosition < window.innerHeight && excPosition > 0) {
+        exc.style.animation = "appear 1s ease-out forwards";
+    } else {
         exc.style.animation = "none";
     }
 
     // Loop through each element in showimg and apply animation
-    showimg.forEach((img) => 
-    {
+    showimg.forEach((img) => {
         const showimgPosition = img.getBoundingClientRect().top;
         if (showimgPosition < window.innerHeight && showimgPosition > 0) {
-            img.style.animation = "appear 1s ease-out forwards";
+            img.style.animation = "entry 1s ease-out forwards";
         } else {
             img.style.animation = "none";
+        }
+    });
+    
+    // Loop through each element in descpara and apply animation
+    descpara.forEach((para) => {
+        const descparaPosition = para.getBoundingClientRect().top;
+        if (descparaPosition < window.innerHeight && descparaPosition > 0) {
+            para.style.animation = "showdown linear";
+        } else {
+            para.style.animation = "none";
         }
     });
 });
